3f116d87296977d664636e3c4d0f3a3f0b766725
Blas Medina <blasmedina5@gmail.com>
2021-05-12 21:35:39 -0300

    register test case

--- test/pageobjects/register.page.js
+++ test/pageobjects/register.page.js
@@ -1,13 +1,46 @@
-const Page = require('./page');
+const Page = require("./page");
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
 class RegisterPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
-    get flashAlert () { return $('#flash') }
-}
+    get fullName() {
+        return $("#fullName");
+    }
+    get email() {
+        return $("#email");
+    }
+    get password() {
+        return $("#password");
+    }
+    get repeatPassword() {
+        return $("#repeatPass");
+    }
+    get submitBtn() {
+        return $("#submitForm");
+    }
+    get wrongNameDiv() {
+        return $("#wName");
+    }
+    get wrongEmailDiv() {
+        return $("#wEmail");
+    }
+    get wrongPasswordDiv() {
+        return $("#wPassword");
+    }
+    get wrongConfirmPasswordDiv() {
+        return $("#wRepeat");
+    }
+    get validationsDiv() {
+        return $("#validations");
+    }
+    get redirectToLogin() {
+        return $("a");
+    }
 
+    open() {
+        return super.open("register");
+    }
+
+    submit() {
+        this.submitBtn.click();
+    }
+}
 module.exports = new RegisterPage();
--- test/specs/register.js
+++ test/specs/register.js
@@ -1,1 +1,29 @@
-const RegisterPage = require('../pageobjects/register.page');
+const RegisterPage = require("../pageobjects/register.page");
+
+describe("Register form credentials verifications", () => {
+  beforeAll("Open browser", () => {
+    RegisterPage.open();
+  });
+  it("Put valid data but invalid full name", () => {
+    RegisterPage.fullName.setValue("juancarlos");
+    RegisterPage.email.setValue("juancarlos@gmail.com");
+    RegisterPage.password.setValue("j1324657");
+    RegisterPage.repeatPassword.setValue("j1324657");
+
+    expect(RegisterPage.wrongNameDiv).toHaveText(
+      "Name should have more than 6 characters and a space between words"
+    );
+  });
+
+  it("Put valid data in the form", () => {
+    RegisterPage.fullName.setValue("Juan Carlos");
+    RegisterPage.email.setValue("juancarlos@gmail.com");
+    RegisterPage.password.setValue("j1324657");
+    RegisterPage.repeatPassword.setValue("j1324657");
+    RegisterPage.submit();
+
+    expect(RegisterPage.validationsDiv).toBeDisplayed(
+      "Registration successfully. User data: juancarlos@gmail.com j1324657"
+    );
+  });
+});
